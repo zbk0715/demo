80e7866ddbf166da44d558c42e7118a5727fc3ed
sx-9703 <sx-cengbk@dtdream.com>
2021-10-08 19:16:27 +0800

    update

--- src/main/java/com/example/demo/configuration/RedisConfig.java
+++ src/main/java/com/example/demo/configuration/RedisConfig.java
@@ -1,7 +1,40 @@
 package com.example.demo.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:test.properties")
+@EnableConfigurationProperties(RedisConfig.class)
+@ConfigurationProperties(prefix = "test")
 public class RedisConfig {
+    private int id;
+
+    private String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "RedisConfig{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
